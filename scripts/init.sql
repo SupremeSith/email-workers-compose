3e87728f2781e6dc1a6401481326df76237b5479
SupremeSith <woof.portocarrero@gmail.com>
2025-10-23 19:53:31 -0300

    Initial setup of the email sending user interface

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,10 +1,10 @@
 create database email_sender;
 
-\c email_sender;
+\c email_sender
 
-create table sent_emails (
-    id serial primary key,
+create table emails (
+    id serial not null,
     data timestamp not null default current_timestamp,
-    assunto varchar(255) not null,
-    mensagem varchar(2500) not null,
+    assunto varchar(100) not null,
+    mensagem varchar(250) not null
 );
